96c88d05272e8d79dcd1ddeff35d954501fb2dcb
elihinshaw <eghinshaw@gmail.com>
2023-08-30 16:43:54 -0500

    crud works

--- locations/views.py
+++ locations/views.py
@@ -12,49 +12,44 @@ from .models import Location
 @csrf_exempt
 def locations(request):
 
-    if (request.method == 'GET'):
+    if request.method == 'GET':
         locations = Location.objects.all()
         serializer = LocationSerializer(locations, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    elif (request.method == 'POST'):
-        try:
-            data = JSONParser().parse(request)
-            serializer = LocationSerializer(data=data)
-        except JSONDecodeError as e:
-            return JsonResponse({"error": "Invalid JSON data: " + str(e)}, status=400)
-
-        if (serializer.is_valid()):
+    elif request.method == 'POST':
+        serializer = LocationSerializer(data=request.data)
+        if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @csrf_exempt
 def location_detail(request, pk):
-    print("Received a DELETE request for location:", pk)  # Debugging line
+    print("Received a DELETE request for location:", pk)
     try:
         location = Location.objects.get(pk=pk)
     except Location.DoesNotExist:
         return HttpResponse(status=404)
 
-    if (request.method == 'GET'):
+    if request.method == 'GET':
         serializer = LocationSerializer(location)
         return JsonResponse(serializer.data)
 
-    elif (request.method == 'PUT'):
+    elif request.method == 'PUT':
         try:
             data = JSONParser().parse(request)
             serializer = LocationSerializer(location, data=data)
         except JSONDecodeError as e:
             return JsonResponse({"error": "Invalid JSON data: " + str(e)}, status=400)
 
-        if (serializer.is_valid()):
+        if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, status=200)
         return JsonResponse(serializer.errors, status=400)
 
-    elif (request.method == 'DELETE'):
+    elif request.method == 'DELETE':
         print("Deleting location:", location)
         location.delete()
         return HttpResponse(status=204)
