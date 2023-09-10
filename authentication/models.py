a5cdfb29c6f3581b1c9830ce24908f85e84e2ef4
elihinshaw <eghinshaw@gmail.com>
2023-09-09 22:57:00 -0500

    Implemented JWT-based authentication for improved security and statelessness in the application, aligning with common best practices.

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class CustomUser(models.Model):
+    username = models.CharField(unique=True, max_length=30)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('delete_user/', views.delete_user, name='delete_user'),
-    path('profile/', views.profile_api, name='profile_api'),
-    path('logout/', views.logout_view, name='logout')
-]
+    path("signup/", views.SignUp.as_view(), name="signup"),
+    path("login/", views.CreateToken.as_view(), name="login"),
+    path("hello/", views.SayHello.as_view(), name="hello"),
+    ]
