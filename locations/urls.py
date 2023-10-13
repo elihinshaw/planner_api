2b1e03cb6bd3049f48d2334b0426ad490614a907
elihinshaw <eghinshaw@gmail.com>
2023-10-13 13:26:11 -0500

    oops lol

--- locations/urls.py
+++ locations/urls.py
@@ -4,8 +4,8 @@ from locations import views
 
 # Endpoints for locations
 urlpatterns = [
-    path('api/get_static_map/', views.get_static_map),
+    path('api/static-map/', views.get_static_map),
     path('api/photos/', views.get_place_photo),
     path('api/places/', views.search_nearby_places),
-    path('api/get_mapbox_token/', views.get_mapbox_token)
+    path('api/mapbox-token/', views.get_mapbox_token)
 ]
